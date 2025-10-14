05cfc4d0d69ea4014fafeee8ca103359ed27d3be
SEBAS0078 <sebascruz0502@gmail.com>
2025-10-13 22:00:21 -0400

    last commit for sprint0

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -12,6 +12,7 @@ export default function Home() {
       <div className={`${styles.page}`}>
         <main className={styles.main}>
           <h1>MiddBin</h1>
+          <p>A Market place for Middlebury college students</p>
         </main>
         <footer className={styles.footer}>CS312 Project Template</footer>
       </div>
