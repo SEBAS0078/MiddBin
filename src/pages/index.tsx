31b6219f6dc629f67cda8417c69d183654b4c4cd
SEBAS0078 <sebascruz0502@gmail.com>
2025-10-18 23:30:25 -0400

    Working on the create listing functionality

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,13 @@
 import Head from "next/head";
+import { useState } from "react";
 import styles from "@/styles/Home.module.css";
+import data from "../../data/seed.json";
+import CreateListing from "./listings/CreateListing";
 
 export default function Home() {
+  const [collection, setCollection] = useState(data);
+  const [createListing, setCreateListing] = useState(false);
+
   return (
     <>
       <Head>
@@ -13,7 +19,27 @@ export default function Home() {
         <main className={styles.main}>
           <h1>MiddBin</h1>
           <p>A Market place for Middlebury college students</p>
+          {!createListing && (
+            <button
+              type="button"
+              className={styles.createButton}
+              onClick={() => setCreateListing(true)}
+            >
+              Create Listing!
+            </button>
+          )}
+          {createListing && (
+            <CreateListing
+              collection={collection}
+              setCollection={setCollection}
+              setCreateListing={setCreateListing}
+            />
+          )}
+          {collection.map((listing) => {
+            return <h1 key={listing.id}>{listing.title}</h1>;
+          })}
         </main>
+
         <footer className={styles.footer}>CS312 Project Template</footer>
       </div>
     </>
